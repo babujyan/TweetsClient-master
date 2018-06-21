38c41e897725d7ac76d9110626202c095d57fe09
babujyan <31545427+babujyan@users.noreply.github.com>
2018-06-21 10:03:09 +0400

    Initial commit

---
+++ TwitterClient/Client.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+
+namespace TwitterClient
+{
+    public static class Client
+    {
+        public static List<Tweet> GetTweets(string hashtag)
+        {
+            var accessToken = "968195499697278977-h8LRnSDbGnVziwazVvgq8OCQyuvcaLH";// ConfigurationManager.AppSettings["accessToken"];
+            var accessTokenSecret = "tweUelFi3qD7NCcpp8pfJ8o4G9aT73xvKsaJ6tGOXQGp4";// ConfigurationManager.AppSettings["accessTokenSecret"];
+            var consumerKey = "FVGwqaJyk1gy5OitrktPCjNiF";// ConfigurationManager.AppSettings["consumerKey"];
+            var consumerSecret = "qQHZT0ngPUaKE6WCrkIZh1zbzhYHbQ1sW7sn0QDmL0ZTzMgxBE";// ConfigurationManager.AppSettings["consumerSecret"];
+
+            var api = new API(accessToken, accessTokenSecret, consumerKey, consumerSecret);
+
+            var response = api.Get("https://api.twitter.com/1.1/search/tweets.json?q=" + hashtag);
+
+            var tweets = new List<Tweet>();
+
+            foreach (var jsonObject in (JArray)response[0].Get("statuses"))
+            {
+                tweets.Add(new Tweet
+                {
+                    Text = (string)jsonObject["text"],
+                    Username = (string)jsonObject["user"]["screen_name"],
+                    Image = (string)jsonObject["user"]["profile_image_url_https"]
+                });
+            }
+
+            return tweets;
+        }
+    }
+}
